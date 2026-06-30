dec3c145ff370efd3a9abb99b576b83d0e3e11b2
owner <owner@localhost>
2026-06-30 17:03:31 +0900

    Wasm動作確認

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn greet(name: &str) -> String {
+    format!("Hello, {}! from Rust", name)
 }
